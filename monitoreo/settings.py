8a6d1a2a30ddda6e5f31f40f920a03eb256c780c
sacrac <carcas@sacrac.info>
2011-07-02 13:49:16 -0600

    setting para cargar el custom css

--- monitoreo/settings.py
+++ monitoreo/settings.py
@@ -65,6 +65,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    PROJECT_DIR + '/templates',
 )
 
 INSTALLED_APPS = (
