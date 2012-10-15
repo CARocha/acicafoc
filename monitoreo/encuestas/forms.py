e272729ed994b8f9935eeb0a3c6976d6f32eb664
Byron Corrales <byroncorrales@gmail.com>
2012-10-15 11:48:19 -0600

    cambiando .po

--- monitoreo/encuestas/forms.py
+++ monitoreo/encuestas/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from django.forms import ModelForm
 from monitoreo.encuestas.models import *
 from monitoreo.lugar.models import *
+from django.utils.translation import ugettext_lazy as _
 
 ANOS_CHOICES = (('', u'Año'), (2009,'2009'), (2010,'2010'),(2011,'2011'),(2012,'2012'),(2013,'2013'),(2014,'2014'))
 CHOICE_OPCION_F = (('', u'Socio'),(1,'Si'),(2,'No'))
@@ -29,5 +30,5 @@ class MonitoreoForm(forms.Form):
     comunidad = forms.ModelMultipleChoiceField(queryset=Comunidad.objects.all(), required=False)
     #grupo = forms.ChoiceField(label = 'Grupo Etnico', choices = CHOICE_ETNICO, required=False)
     grupo = forms.MultipleChoiceField(choices = CHOICE_ETNICO, required=False)
-    sexo = forms.ChoiceField(choices = CHOICE_SEXO, required=False)
-    dueno = forms.ChoiceField(label = 'Dueño', choices = CHOICE_DUENO_F , required=False, initial=u"Dueño")
+    sexo = forms.ChoiceField(choices = CHOICE_SEXO,label = _('Sexo beneficiario'), required=False)
+    dueno = forms.ChoiceField(label = _('Dueño'), choices = CHOICE_DUENO_F , required=False, initial=u"Dueño")
