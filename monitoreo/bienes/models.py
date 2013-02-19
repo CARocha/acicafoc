a52b09a9e08c71e0240ab182549bdf4ddfd3c8d9
sacrac <crocha09.09@gmail.com>
2013-02-19 14:32:33 -0600

    fix unicode

--- monitoreo/bienes/models.py
+++ monitoreo/bienes/models.py
@@ -160,8 +160,8 @@ class Transporte(models.Model):
     numero = models.IntegerField(null=True, blank=True)
     encuesta = models.ForeignKey(Encuesta)
     
-    def __unicode__(self):
-        return u'%s' % self.transporte.nombre
+    #def __unicode__(self):
+    #    return u'%s' % self.transporte.nombre
     
     class Meta:
         verbose_name_plural = "Medio de transporte"
